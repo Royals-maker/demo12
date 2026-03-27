b4a6dcddbb26e427576e5ada087f034e8018b729
Royals-maker <roystudent786@gmail.com>
2026-03-27 15:37:57 +0530

    Update oddeven.java

--- oddeven.java
+++ oddeven.java
@@ -1,21 +1,8 @@
-package oddeven;
-import java.util.Scanner;
 public class oddeven {
 	public static void main(String[] args)
 {
-	int n;
-	Scanner sc = new Scanner(System.in);
-	System.out.println("Enter the number you want to check:");
-	n = sc.nextInt();
+		System.out.println("helloworld");	
 	
-	if (n % 2 == 0)
-	{
-		System.out.println("the given number "+n+" is Even ");	
-	}
-	else
-	{
-		System.out.println("the given number "+n+" is odd ");	
-	}
 }
 
 }	
